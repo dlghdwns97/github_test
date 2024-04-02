4dd2d7a7c2e26708f3bc70f229bbeeb732d31049
dlghdwns97 <dlghdwns0219@gmail.com>
2024-04-02 12:57:48 +0900

    Hello, World 추가

--- Program.cs
+++ Program.cs
@@ -1,4 +1,2 @@
 ﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World! 2");
-Console.WriteLine("Hello, World! 3");
-Console.WriteLine("World, Hello! 4");
+Console.WriteLine("Hello, World!");
